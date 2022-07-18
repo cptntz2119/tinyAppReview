9e452d0f89f7d485a65c2610634c6ff9c8fbe225
Dong Zhang <dongzhang@Dongs-MacBook-Pro.local>
2022-07-18 16:23:34 -0700

    add function for delete urls

--- express_server.js
+++ express_server.js
@@ -1,12 +1,18 @@
 const express = require("express");
 const app = express();
 const PORT = 8080;
+const bodyParser = require("body-parser");
 
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
+};
+
+const generateRandonString = () => {
+  return Math.random().toString(36).replace("0.", "").substring(0, 6);
 };
 
 app.get("/", (req, res) => {
@@ -27,6 +33,20 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  const shortURL = generateRandonString();
+  console.log(req.body, shortURL);
+  const templateVars = {
+    id: shortURL,
+    longURL: req.body.longURL,
+  };
+  res.render("urls_show", templateVars);
+});
+
 app.get("/urls/:id", (req, res) => {
   // console.log(req.params);
   // console.log(res);
@@ -35,6 +55,19 @@ app.get("/urls/:id", (req, res) => {
     longURL: urlDatabase[req.params.id],
   };
   res.render("urls_show", templateVars);
+});
+
+app.post("/urls/:id/delete", (req, res) => {
+  let removeID = req.params.id;
+
+  console.log(removeID);
+  delete urlDatabase[removeID];
+  res.redirect("/urls");
+});
+
+app.get("/u/:id", (req, res) => {
+  const longURL = urlDatabase[req.params.id];
+  res.redirect(longURL);
 });
 
 app.listen(PORT, () => {
